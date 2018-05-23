c6f5dca09d60e92df4a1d4bb3eca6f8df4f8afb6
Matt Gercz <matt.gercz@healthmyne.com>
2018-05-23 11:37:01 -0500

    started main file

---
+++ mattygboy.c
@@ -0,0 +1,10 @@
+#include "register_structures.h"
+
+#define sRAM 8192 // 8 kb of sRAM
+#define vRAM 8192 // 8 kb of vRAM
+#define ROM 256 // 256 bytes on-CPU ROM
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
