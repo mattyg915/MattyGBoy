408f8b22e2034dbe418e7e561b804f9b42becafe
Matt Gercz <matt.gercz@healthmyne.com>
2018-05-25 14:51:13 -0500

    sketching out main method w comments, starting to declare needed helper functions

--- mattygboy.c
+++ mattygboy.c
@@ -1,10 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "register_structures.h"
 
 #define MEMORY 0xFFFF // Top end of address range
 
-Registers *regs; // Global pointer to registers
-Pointers *ptrs; // Global pointer to stack pointer and program counter
-
+Registers *regs; // Pointer to registers
+Pointers *ptrs; // Pointer to stack pointer and program counter
+unsigned char *memory // Pointer to the beginning of the address space
 /**
  * Prints the contents of the registers and pointers
  * to the console
@@ -19,7 +21,30 @@ void dump_registers() {
 	return;
 }
 
+/**
+ * Takes an opcode and determines its generalized instruction
+ * (e.g. this is a 'load' instruction), then
+ * calls the appropriate method to further decode and emulate it
+ * @param opcode is the 1-byte opcode to decode
+ */
+void initial_decode(unsigned char opcode) {
+	// TODO:this whole method
+	switch (opcode) {
+		
+	}
+}
+
 int main(int argc, char *argv[]) {
+	// Virtual registers are loaded
+	regs = init_registers();
+	ptrs = init_pointers();
+
+	// TODO:Initialize virtual memory
+	memory = malloc(MEMORY);
+	// TODO:Load cartridge data into virtual memory
 	
+	// TODO:Main program loop, fetch/decode/execute
+	
+	free(regs); free(ptrs); free(memory);
 	return 0;
 }
