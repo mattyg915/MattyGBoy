f0691748c85113dcd9848d20e3b0d8764b5af86a
Matt Gercz <matt.gercz@icloud.com>
2018-06-07 10:27:42 -0500

    makeup commit

--- include/register_structures.h
+++ include/register_structures.h
@@ -19,18 +19,28 @@
 #ifndef REGISTERSTRUCTURES
 #define REGISTERSTRUCTURES
 
-typedef struct Registers {
-	// F register is used for flags, IME is the Interrupt Master Enable
-	unsigned char A, B, C, D, E, F, H, L, IME;
+typedef struct Registers 
+{
+	// F register is used for flags
+	unsigned char A, B, C, D, E, F, H, L;
 } Registers;
 
-typedef struct Pointers {
+typedef struct Pointers 
+{
 	// Two 16-bit registers are used for stack pointer and program counter
 	unsigned short SP, PC;
 } Pointers;
 
-Registers* init_registers();
+typedef struct CPU_Flags 
+{
+	// Special flags used only by CPU
+	// jumped set when an instruction sets the PC and cleared after each cyycle
+	// IME is the Interrupt Master Enable flag
+	unsigned char jumped, IME;
+} CPU_Flags;
 
+Registers* init_registers();
+CPU_Flags* init_flags();
 Pointers* init_pointers();
 
 #endif
